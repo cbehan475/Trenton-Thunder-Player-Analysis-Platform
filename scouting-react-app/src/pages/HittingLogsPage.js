7526a635bbebca6acb33e3d29e7deb7901d9d3ae
Your NameChris-Behan <chrisbehan15@gmail.com>
2025-07-25 15:13:06 -0400

    changed hitting log 6-10 to correct date

--- scouting-react-app/src/pages/HittingLogsPage.js
+++ scouting-react-app/src/pages/HittingLogsPage.js
@@ -5,7 +5,7 @@ import GameDateDropdown from '../components/GameDateDropdown';
 import HitterDropdown from '../components/HitterDropdown';
 import InningDropdown from '../components/InningDropdown';
 
-const GAME_DATES = ['2025-06-04', '2025-06-05', '2025-06-06', '2025-06-07', '2025-06-08']; // Added June 8, 2025 for dropdown and data loading.
+const GAME_DATES = ['2025-06-04', '2025-06-05', '2025-06-06', '2025-06-07', '2025-06-08', '2025-06-10']; // Corrected to June 10, 2025 for dropdown and data loading.
 
 export default function HittingLogsPage() {
   const [selectedDate, setSelectedDate] = useState(GAME_DATES[0]);
